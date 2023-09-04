9a0cd0c81678e8f47ea5c16c28ad9a4d678312d7
EngMostafa <EngMostafa@engineering.com>
2023-09-04 14:36:30 +0300

    simple as pssible

---
+++ simple_loop.py
@@ -0,0 +1,8 @@
+def main():
+    n_time=0
+    times=int(input("enter number of time you want to replay: "))
+    while times>n_time:
+        print(f"hello everyone {n_time+1} times")
+        n_time+=1
+        
+main()
